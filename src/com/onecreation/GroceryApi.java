35caeff5c77b4dc2e311d1997093be3d35ea6687
Dennis Adjei-Baah <adjeibaah@etsu.edu>
2017-01-04 22:42:32 -0500

    init commit

---
+++ src/com/onecreation/GroceryApi.java
@@ -0,0 +1,14 @@
+package com.onecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dennis on 1/4/17.
+ */
+@SpringBootApplication
+public class GroceryApi {
+    public static void main(String[] args){
+        SpringApplication.run(GroceryApi.class, args);
+    }
+}
