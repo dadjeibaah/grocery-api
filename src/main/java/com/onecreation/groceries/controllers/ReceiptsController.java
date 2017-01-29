5e4baa4a5867700ff949fe33c83703bd49825a19
Dennis Adjei-Baah <adjeibaah@etsu.edu>
2017-01-28 23:11:26 -0500

    adds model and database support

--- src/main/java/com/onecreation/groceries/controllers/ReceiptsController.java
+++ src/main/java/com/onecreation/groceries/controllers/ReceiptsController.java
@@ -1,6 +1,7 @@
 package com.onecreation.groceries.controllers;
 
 import com.onecreation.groceries.models.GroceryList;
+import com.onecreation.groceries.models.ReceiptItems;
 import com.onecreation.groceries.services.ReceiptScannerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by dennis on 1/10/17.
@@ -27,10 +29,15 @@ public class ReceiptsController {
         this.receiptScannerService = receiptScannerService;
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value ="/images",method = RequestMethod.POST)
     public ResponseEntity<Object> getGroceryListFromReceipt(@RequestBody MultipartFile file) throws IOException {
         GroceryList receipt = receiptScannerService.getReceiptInformation(file);
         return new ResponseEntity<>(receipt, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Object> saveReceiptList(@RequestBody List<ReceiptItems> listOfReceiptItems){
+        return new ResponseEntity<>(listOfReceiptItems, HttpStatus.OK);
     }
 
 }
